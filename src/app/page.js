a1bdab26f6e9f9a29aa10662f51c1fa699725371
hasibulhasananik40420 <hasibulhasananik40420@gmail.com>
2023-12-17 19:24:21 +0600

    added

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
+import Feedback from '@/components/Feedback'
 import Header from '@/components/Header'
 
 const HomePage = () => {
   return (
     <div className='overflow-hidden'>
       <Header/>
+      <Feedback/>
        
     </div>
   )
---
+++ src/components/Feedback.js
@@ -0,0 +1,15 @@
+"use client"
+
+import FeedbackCard from "./FeedbackCard"
+
+const Feedback = () => {
+  return (
+    <section className='max-w-[1320px] mx-auto' >
+  
+    <FeedbackCard/>
+
+   </section>
+  )
+}
+
+export default Feedback
