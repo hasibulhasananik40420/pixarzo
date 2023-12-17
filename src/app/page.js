ca88a9104637d761dc54601e940a99e033c4e2d6
hasibulhasananik40420 <hasibulhasananik40420@gmail.com>
2023-12-17 19:39:11 +0600

    added

--- src/app/page.js
+++ src/app/page.js
@@ -4,12 +4,18 @@ import GoogleRedesign from '@/components/GoogleRedesign'
 import Header from '@/components/Header'
 import LetestProjects from '@/components/LetestProjects'
 import OurClient from '@/components/OurClient'
+import OurTeam from '@/components/OurTeam'
+import Services from '@/components/Services'
+import SlientSeller from '@/components/SlientSeller'
 import SteepProcess from '@/components/SteepProcess'
 
 const HomePage = () => {
   return (
     <div className='overflow-hidden'>
       <Header/>
+      <Services/>
+      <OurTeam/>
+      <SlientSeller/>
       <LetestProjects/>
       <SteepProcess/>
       <GoogleRedesign/>
