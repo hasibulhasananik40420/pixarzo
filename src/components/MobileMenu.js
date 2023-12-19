dca61dad04e7aa7eb1a99da60115f9cdbc200f25
hasibulhasananik40420 <hasibulhasananik40420@gmail.com>
2023-12-19 20:15:48 +0600

    done

--- src/components/MobileMenu.js
+++ src/components/MobileMenu.js
@@ -1,14 +1,13 @@
-"use client";
+"use client"
 
 import { useEffect, useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
 import Logo from "./Logo";
 import { CiMenuFries } from "react-icons/ci";
 import { IoMdClose } from "react-icons/io";
 import Link from "next/link";
 
 const MobileMenu = () => {
-
-  
   const [isFixed, setIsFixed] = useState(false);
   const [show, setShow] = useState(false);
 
@@ -32,109 +31,80 @@ const MobileMenu = () => {
     };
   }, [isFixed]); // Re-run effect only when isFixed changes
 
+  const sidebarVariants = {
+    hidden: { x: "-100%" },
+    visible: { x: 0 },
+  };
+
   return (
-      // mobile menu for small device start here 
-    <section
-      className={`${
-        isFixed ? "fixed top-0" : ""
-      } bg-PrimaryColor h-[96px] px-3 transition-all ease-in-out duration-500 w-full z-30 `}
-    >
+    <section className={`${isFixed ? "fixed top-0" : ""} bg-PrimaryColor h-[96px] px-3 transition-all ease-in-out duration-500 w-full z-30 `}>
       <div className="flex justify-between items-center h-[96px] px-3">
         <Logo />
         <div className="">
-          <CiMenuFries
-            onClick={() => setShow(true)}
-            className="text-3xl text-white cursor-pointer"
-          />
+          <CiMenuFries onClick={() => setShow(true)} className="text-3xl text-white cursor-pointer" />
         </div>
       </div>
 
-      {show && (
-        <>
-          <div
-            className="fixed top-0 left-0 w-full h-full bg-black opacity-50 z-20"
-            onClick={() => setShow(false)}
-          />
+      <AnimatePresence>
+        {show && (
+          <>
+            <motion.div
+              className="fixed top-0 left-0 w-full h-full  z-20"
+              onClick={() => setShow(false)}
+              initial="hidden"
+              animate="visible"
+              exit="hidden"
+              variants={sidebarVariants}
+              transition={{ duration: 0.5 }}
+            />
 
-          <div className="absolute lg:hidden xl:hidden top-0 right-0 w-full h-screen flex flex-col items-start">
-            <div className="w-[100%] h-full bg-[#112240] flex flex-col z-30 relative pt-20 px-8 md:px-12">
-              <IoMdClose
-                onClick={() => setShow(false)}
-                className="text-3xl cursor-pointer text-red-500 absolute top-4 right-4 duration-300"
-              />
+            <motion.div
+              className="absolute lg:hidden xl:hidden top-0 right-0 w-full h-screen flex flex-col items-start"
+              variants={sidebarVariants}
+              initial="hidden"
+              animate="visible"
+              exit="hidden"
+              transition={{ duration: 0.5 }}
+            >
+              <div className="w-[100%] h-full bg-[#112240] flex flex-col z-30 relative pt-20 px-8 md:px-12">
+                <IoMdClose onClick={() => setShow(false)} className="text-3xl cursor-pointer text-red-500 absolute top-4 right-4 duration-300" />
 
-                {/*************** navbar menu item start here *********************/}
-              <div className="flex flex-col gap-8">
-                <div>
-                  <Link onClick={() => setShow(false)}
-                    className="h-7 overflow-x-hidden overflow-y-hidden inline-flex relative group"
-                    href={"/"}
-                  >
-                    <h1 className="text-[24px] text-white font-medium leading-[26px] group-hover:text-[#FF338F]">
-                      Home
-                    </h1>
-                    <span className="absolute w-full h-[2px] bg-[#FF338F] left-0 bottom-0 -translate-x-[110%] group-hover:translate-x-0 transition-transform duration-500"></span>
-                  </Link>
-                </div>
+                <div className="flex flex-col gap-8">
+                  
+                    <Link onClick={() => setShow(false)} href={"/"}>
+                      <h1 className="text-[24px] text-white font-medium leading-[26px] ">Home</h1>
+                      
+                    </Link>
+                  
+                   <Link onClick={() => setShow(false)} href={"/"}>
+                      <h1 className="text-[24px] text-white font-medium leading-[26px] ">Case Study</h1>
+                      
+                    </Link>
+                    <Link onClick={() => setShow(false)} href={"/"}>
+                      <h1 className="text-[24px] text-white font-medium leading-[26px] "> About Us</h1>
+                      
+                    </Link>
+                    <Link onClick={() => setShow(false)} href={"/"}>
+                      <h1 className="text-[24px] text-white font-medium leading-[26px] ">Blog</h1>
+                      
+                    </Link>
+                    <Link onClick={() => setShow(false)} href={"/"}>
+                      <h1 className="text-[24px] text-white font-medium leading-[26px] ">Contact Us</h1>
+                      
+                    </Link>
+                  
 
-                <div>
-                  <Link onClick={() => setShow(false)}
-                    className="h-7 overflow-x-hidden overflow-y-hidden inline-flex relative group"
-                    href={"/"}
-                  >
-                    <h1 className="text-[24px] text-white font-medium leading-[26px] group-hover:text-[#FF338F]">
-                      Case Study
-                    </h1>
-                    <span className="absolute w-full h-[2px] bg-[#FF338F] left-0 bottom-0 -translate-x-[110%] group-hover:translate-x-0 transition-transform duration-500"></span>
-                  </Link>
-                </div>
-
-                <div>
-                  <Link onClick={() => setShow(false)}
-                    className="h-7 overflow-x-hidden overflow-y-hidden inline-flex relative group"
-                    href={"/"}
-                  >
-                    <h1 className="text-[24px] text-white font-medium leading-[26px] group-hover:text-[#FF338F]">
-                      About Us
-                    </h1>
-                    <span className="absolute w-full h-[2px] bg-[#FF338F] left-0 bottom-0 -translate-x-[110%] group-hover:translate-x-0 transition-transform duration-500"></span>
-                  </Link>
-                </div>
-
-                <div>
-                  <Link onClick={() => setShow(false)}
-                    className="h-7 overflow-x-hidden overflow-y-hidden inline-flex relative group"
-                    href={"/"}
-                  >
-                    <h1 className="text-[24px] text-white font-medium leading-[26px] group-hover:text-[#FF338F]">
-                      Blog
-                    </h1>
-                    <span className="absolute w-full h-[2px] bg-[#FF338F] left-0 bottom-0 -translate-x-[110%] group-hover:translate-x-0 transition-transform duration-500"></span>
-                  </Link>
-                </div>
-
-                <div>
-                  <Link onClick={() => setShow(false)}
-                    className="h-7 overflow-x-hidden overflow-y-hidden inline-flex relative group"
-                    href={"/"}
-                  >
-                    <h1 className="text-[24px] text-white font-medium leading-[26px] group-hover:text-[#FF338F]">
-                      Contact Us
-                    </h1>
-                    <span className="absolute w-full h-[2px] bg-[#FF338F] left-0 bottom-0 -translate-x-[110%] group-hover:translate-x-0 transition-transform duration-500"></span>
-                  </Link>
+                 
+                  
                 </div>
               </div>
-               {/*************** navbar menu item end here *********************/}
-
-
-            </div>
-          </div>
-        </>
-      )}
+            </motion.div>
+          </>
+        )}
+      </AnimatePresence>
     </section>
-     // mobile menu for small device end here 
   );
 };
 
 export default MobileMenu;
+
